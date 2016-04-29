54eecbb31387fc9cd14a6d4c5e02c7c0d65b086e
Quan Wang <BetterWang@gmail.com>
2016-04-29 17:36:06 +0200

    [!] config

--- test/crab_HIMB_v1.py
+++ test/crab_HIMB_v1.py
@@ -5,17 +5,18 @@ from httplib import HTTPException
 
 config = config()
 
-config.General.requestName = 'HIMB3_QVector_v3'
+config.General.requestName = 'HIMB3_QVector_v4'
 config.General.workArea = 'CrabArea'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'qw_PbPb15_HIMB.py'
 #config.JobType.inputFiles = ['PbPb_dijet_TT_5TeV_v2.root']
-config.Data.inputDataset = '/HIMinimumBias3/HIRun2015-PromptReco-v1/AOD'
-#config.Data.inputDBS = 'phys03'
+config.Data.inputDataset =  '/HIMinimumBias3/qwang-HIMinBias_v2-ce439b1c24fa1bf3a491f2ccb0fd72a9/USER'
+#config.Data.inputDataset = '/HIMinimumBias3/HIRun2015-PromptReco-v1/AOD'
+config.Data.inputDBS = 'phys03'
 config.Data.splitting = 'LumiBased'
-config.Data.unitsPerJob = 50
+config.Data.unitsPerJob = 25
 config.Data.outLFNDirBase = '/store/group/phys_heavyions/qwang/QVector/'
 config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/HI/Cert_262548-263757_PromptReco_HICollisions15_JSON_v2.txt'
 config.Data.publication = False
