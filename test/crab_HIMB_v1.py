254923b4149b93736e26d1d12c757c0f0d37866f
Quan Wang <BetterWang@gmail.com>
2016-06-08 15:39:40 +0200

    [+] cfg

--- test/crab_HIMB_v1.py
+++ test/crab_HIMB_v1.py
@@ -5,7 +5,7 @@ from httplib import HTTPException
 
 config = config()
 
-config.General.requestName = 'HIMB3_QVector_v4'
+config.General.requestName = 'HIMB3_QVector_v5'
 config.General.workArea = 'CrabArea'
 config.General.transferOutputs = True
 config.General.transferLogs = True
